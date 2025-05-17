e72f8a3ad84002cb809a118419a3abfbd2381aa4
Soban Syeed <Soban.syeed@outlook.com>
2025-05-17 09:15:31 +0530

    Make the Robot follow Player

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Robot : MonoBehaviour
 {
+    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,6 @@ public class Robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
