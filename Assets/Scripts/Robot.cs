f0a4010c8e45a20be2516a091a6849b36941a8c9
Soban Syeed <Soban.syeed@outlook.com>
2025-07-12 16:19:26 +0530

    Add Player Follow script

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,16 +1,27 @@
+using StarterAssets;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Robot : MonoBehaviour
 {
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = FindFirstObjectByType<FirstPersonController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 }
