c9ba066e72f5dd85267f83afecb327c6eeb18c3b
Soban Syeed <Soban.syeed@outlook.com>
2025-07-22 09:29:41 +0530

    Add Raycast for pistol

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,16 +1,27 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    StarterAssetsInputs starterAssetsInputs;
+
+    void Awake()
     {
-        
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (starterAssetsInputs.shoot)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+                starterAssetsInputs.ShootInput(false); // Reset shoot input after processing
+            }
+        }
     }
 }
--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool shoot;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,10 +44,15 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnShoot(InputValue value)
+		{
+			ShootInput(value.isPressed);
+        }
 #endif
 
 
-		public void MoveInput(Vector2 newMoveDirection)
+        public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
 		} 
@@ -65,6 +71,11 @@ namespace StarterAssets
 		{
 			sprint = newSprintState;
 		}
+
+		public void ShootInput(bool newShootState)
+		{
+			shoot = newShootState;
+        }
 		
 		private void OnApplicationFocus(bool hasFocus)
 		{
