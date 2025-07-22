376308a4cea746a076433bc5db56705cb9e1bc09
Soban Syeed <Soban.syeed@outlook.com>
2025-07-22 10:45:36 +0530

    Add Shoot animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damage = 1;
 
     StarterAssetsInputs starterAssetsInputs;
+
+    string SHOOT_ANIMATION = "Shoot";
 
     void Awake()
     {
@@ -24,13 +27,13 @@ public class Weapon : MonoBehaviour
         if (!starterAssetsInputs.shoot) return;
 
         muzzleFlash.Play();
+        animator.Play(SHOOT_ANIMATION, 0, 0f);
         RaycastHit hit;
+        starterAssetsInputs.ShootInput(false); // Reset shoot input after processing
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            hit.collider.GetComponent<EneemyHealth>()?.TakeDamage(damage);
-        
-            starterAssetsInputs.ShootInput(false); // Reset shoot input after processing
+            hit.collider.GetComponent<EneemyHealth>()?.TakeDamage(damage);        
         }
     }
 }
