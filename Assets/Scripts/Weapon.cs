59f7077dfd763365805fdc037949937bb6df5639
Soban Syeed <Soban.syeed@outlook.com>
2025-07-29 20:29:54 +0530

    Update weapon script to use weaponSO

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] WeaponSO weaponSO;
     [SerializeField] Animator animator;
     [SerializeField] GameObject hitVFXPrefab;
     [SerializeField] ParticleSystem muzzleFlash;
-    [SerializeField] int damage = 1;
 
     StarterAssetsInputs starterAssetsInputs;
 
@@ -35,7 +35,7 @@ public class Weapon : MonoBehaviour
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
-            hit.collider.GetComponent<EneemyHealth>()?.TakeDamage(damage);
+            hit.collider.GetComponent<EneemyHealth>()?.TakeDamage(weaponSO.Damage);
         }
     }
 }
