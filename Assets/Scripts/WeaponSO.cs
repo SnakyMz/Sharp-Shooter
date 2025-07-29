138d0530a450c4fc0734496081816e2ae90ba0b2
Soban Syeed <Soban.syeed@outlook.com>
2025-07-29 22:42:38 +0530

    Add MachineGun Prefab

--- Assets/Scripts/WeaponSO.cs
+++ Assets/Scripts/WeaponSO.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class WeaponSO : ScriptableObject
 {
     public int Damage = 1;
-    public float FireRate = 0.4f;
+    public float FireRate = 0.3f;
     public GameObject HitVFX;
+    public bool IsAutomatic = false;
 }
