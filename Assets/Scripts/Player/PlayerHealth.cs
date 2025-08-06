1db397767a6cabb31f8b7833ce9bfe63460025c3
Soban Syeed <Soban.syeed@outlook.com>
2025-08-06 14:05:05 +0530

    Add health bars display

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,13 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] Image[] shieldBars;
     [SerializeField] CinemachineCamera deathCamera;
     [SerializeField] Transform weaponCamera;
-    [SerializeField] int health = 5;
+    [SerializeField] int health = 10;
 
     int currentHealth;
     int deathCameraPriiority = 20;
@@ -15,9 +17,25 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = health;
     }
 
+    void AdjustShield()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].enabled = true;
+            }
+            else
+            {
+                shieldBars[i].enabled = false;
+            }
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        AdjustShield();
 
         if (currentHealth <= 0)
         {
